24ee62c24a086020ffe4fd98e93f12569f040c97
JeffBassP <jeferson_jfs@hotmail.com>
2020-07-17 12:17:39 -0300

    Acerto de resposividade e adicao de containers

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jul 15, 2020 15:37:25").getTime();
+var countDownDate = new Date("Jul 23, 2020 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
